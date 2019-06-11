16cc4891f9adcfecf243bc0785cfaf6d63b28d14
Benjamin Chin <benjamin.d.chin@gmail.com>
2019-06-11 10:30:46 -0400

    coordinates

--- Card.java
+++ Card.java
@@ -3,7 +3,8 @@ public class Card
     private int rank; // 
     private String suit;
     private String color;
-
+    private int x;
+    private int y;
     public Card(int r, String s, String c)
     {
         rank = r;
@@ -29,5 +30,143 @@ public class Card
     public String toString()
     {
         return rank + " " + suit + " " + color;
+    }
+    //Calculate sprite coordinate x
+    public void getCoordinates()
+    {
+        if(color == "RED")
+        {
+            if(suit == "HEARTS")
+            {
+                switch(rank)
+                {
+                    case 1:
+                        x = 1;
+                    case 2:
+                        x = 2;
+                    case 3:
+                        x = 3;
+                    case 4:
+                        x = 4;
+                    case 5:
+                        x = 5;
+                    case 6:
+                        x = 6;
+                    case 7:
+                        x = 7;
+                    case 8:
+                        x = 8;
+                    case 9:
+                        x = 9;
+                    case 10:
+                        x = 10;
+                    case 11:
+                        x = 11;
+                    case 12:
+                        x = 12;
+                    case 13:    
+                        x = 13;
+                }
+            }
+            else if(suit == "DIAMOND")
+            {
+                switch(rank)
+                {
+                    case 1:
+                        x = 1;
+                    case 2:
+                        x = 2;
+                    case 3:
+                        x = 3;
+                    case 4:
+                        x = 4;
+                    case 5:
+                        x = 5;
+                    case 6:
+                        x = 6;
+                    case 7:
+                        x = 7;
+                    case 8:
+                        x = 8;
+                    case 9:
+                        x = 9;
+                    case 10:
+                        x = 10;
+                    case 11:
+                        x = 11;
+                    case 12:
+                        x = 12;
+                    case 13:    
+                        x = 13;
+                }
+            }
+        }
+        else if(color == "BLACK")
+        {
+            if(suit == "SPADES")
+            {
+                switch(rank)
+                {
+                    case 1:
+                        x = 1;
+                    case 2:
+                        x = 2;
+                    case 3:
+                        x = 3;
+                    case 4:
+                        x = 4;
+                    case 5:
+                        x = 5;
+                    case 6:
+                        x = 6;
+                    case 7:
+                        x = 7;
+                    case 8:
+                        x = 8;
+                    case 9:
+                        x = 9;
+                    case 10:
+                        x = 10;
+                    case 11:
+                        x = 11;
+                    case 12:
+                        x = 12;
+                    case 13:    
+                        x = 13;
+                }
+            }
+            else if(suit == "CLUBS")
+            {
+                switch(rank)
+                {
+                    case 1:
+                        x = 1;
+                    case 2:
+                        x = 2;
+                    case 3:
+                        x = 3;
+                    case 4:
+                        x = 4;
+                    case 5:
+                        x = 5;
+                    case 6:
+                        x = 6;
+                    case 7:
+                        x = 7;
+                    case 8:
+                        x = 8;
+                    case 9:
+                        x = 9;
+                    case 10:
+                        x = 10;
+                    case 11:
+                        x = 11;
+                    case 12:
+                        x = 12;
+                    case 13:    
+                        x = 13;
+                }
+            }
+        }
     }
 }
