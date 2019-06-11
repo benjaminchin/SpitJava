cefdcbb47b429c44c3f0fb7b077356d594878f35
Benjamin Chin <benjamin.d.chin@gmail.com>
2019-06-11 07:47:41 -0400

    YO

--- PlayGame.java
+++ PlayGame.java
@@ -9,6 +9,33 @@ public class PlayGame
 
     public static void makePlay()
     {
+        if() //input is 1
+        {
+            if() //input 2 is Q OR 0 AND selected card is +- 1 to the top card of pile Q/0
+            {
+            }
+            else //input2 is E OR . AND selected card is +- 1 to the top card of pile E/.
+            {
+            }
+        }
+        else if() //input is 2
+        {
+        
+        }
+        else if() //input is 3
+        {
+        
+        }
+        else if() //input is 4
+        {
+        
+        }
+        else if() //input is 5
+        {
+        
+        }
+        
+        
         
     }
     
