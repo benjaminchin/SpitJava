7f2c7cd56338c014e75fb1f7f04b884710dab6b1
Benjamin Chin <benjamin.d.chin@gmail.com>
2019-06-13 10:11:28 -0400

    Repaint testing

--- DisplayImage.java
+++ DisplayImage.java
@@ -11,26 +11,77 @@ import java.awt.Color;
 public class DisplayImage extends Deck
 {
     private static JLabel player1 = new JLabel();
+    private static JLabel oneStockOne = new JLabel();
+    private static JLabel oneStockTwo = new JLabel();
+    private static JLabel oneStockThree = new JLabel();
+    private static JLabel oneStockFour = new JLabel();
+    private static JLabel oneStockFive = new JLabel();
+        
+    private static JLabel player2 = new JLabel();
+    private static JLabel twoStockOne = new JLabel();
+    private static JLabel twoStockTwo = new JLabel();
+    private static JLabel twoStockThree = new JLabel();
+    private static JLabel twoStockFour = new JLabel();
+    private static JLabel twoStockFive = new JLabel();
+        
+    private static JLabel leftSpit = new JLabel();
+    private static JLabel rightSpit = new JLabel();
+    
+    private static BufferedImage img;
+    private static Deck deck = new Deck();
     public static void main(String[] args) throws IOException
     {
-        Deck deck = new Deck();
+        img = ImageIO.read(new File("cards.png"));
         deck.shuffle();
+        deck.deal();
         DisplayImage abc = new DisplayImage();
-        repaintCard(deck.get(1).getX, );
-    }
-    public static void repaintCard(int x, int y) throws IOException
-    {
-        BufferedImage img = ImageIO.read(new File("cards.png"));
-        BufferedImage swapCard = img.getSubimage(936, 0, 72, 96); //Closed face
+        repaint();
         
-        player1.setIcon(new ImageIcon(swapCard));
-        player1.setBounds(x, y, 72, 96);
+    }
+    
+    public static void repaint()
+    {
+        //BufferedImage img = ImageIO.read(new File("cards.png"));
+        BufferedImage swapPlayer1 = img.getSubimage(deck.player1Cards.get(0).getX(), deck.player1Cards.get(0).getY(), 72, 96);
+        BufferedImage swapOneStockOne = img.getSubimage(deck.OneStockOne.get(0).getX(), deck.OneStockOne.get(0).getX(), 72, 96);
+        BufferedImage swapOneStockTwo = img.getSubimage(deck.OneStockTwo.get(0).getX(), deck.OneStockTwo.get(0).getX(), 72, 96);
+        BufferedImage swapOneStockThree = img.getSubimage(deck.OneStockThree.get(0).getX(), deck.OneStockThree.get(0).getX(), 72, 96);
+        BufferedImage swapOneStockFour = img.getSubimage(deck.OneStockFour.get(0).getX(), deck.OneStockFour.get(0).getX(), 72, 96);
+        BufferedImage swapOneStockFive = img.getSubimage(deck.OneStockFive.get(0).getX(), deck.OneStockFive.get(0).getX(), 72, 96);
+        
+        BufferedImage swapPlayer2 = img.getSubimage(deck.player2Cards.get(0).getX(), deck.player2Cards.get(0).getY(), 72, 96);
+        BufferedImage swapTwoStockOne = img.getSubimage(deck.TwoStockOne.get(0).getX(), deck.TwoStockOne.get(0).getY(), 72, 96);
+        BufferedImage swapTwoStockTwo = img.getSubimage(deck.TwoStockTwo.get(0).getX(), deck.TwoStockTwo.get(0).getY(), 72, 96);
+        BufferedImage swapTwoStockThree = img.getSubimage(deck.TwoStockThree.get(0).getX(), deck.TwoStockThree.get(0).getY(), 72, 96);
+        BufferedImage swapTwoStockFour = img.getSubimage(deck.TwoStockFour.get(0).getX(), deck.TwoStockFour.get(0).getY(), 72, 96);
+        BufferedImage swapTwoStockFive = img.getSubimage(deck.TwoStockFive.get(0).getX(), deck.TwoStockFive.get(0).getY(), 72, 96);
+        
+        BufferedImage swapLeftSpit = img.getSubimage(deck.LeftSpit.get(0).getX(), deck.LeftSpit.get(0).getY(), 72, 96);
+        BufferedImage swapRightSpit = img.getSubimage(deck.RightSpit.get(0).getX(), deck.RightSpit.get(0).getY(), 72, 96);
+        
+        player1.setIcon(new ImageIcon(swapPlayer1));
+        oneStockOne.setIcon(new ImageIcon(swapOneStockOne));
+        oneStockTwo.setIcon(new ImageIcon(swapOneStockTwo));
+        oneStockThree.setIcon(new ImageIcon(swapOneStockThree));
+        oneStockFour.setIcon(new ImageIcon(swapOneStockFour));
+        oneStockFive.setIcon(new ImageIcon(swapOneStockFive));
+        
+        player2.setIcon(new ImageIcon(swapPlayer1));
+        twoStockOne.setIcon(new ImageIcon(swapTwoStockOne));
+        twoStockTwo.setIcon(new ImageIcon(swapTwoStockTwo));
+        twoStockThree.setIcon(new ImageIcon(swapTwoStockThree));
+        twoStockFour.setIcon(new ImageIcon(swapTwoStockFour));
+        twoStockFive.setIcon(new ImageIcon(swapTwoStockFive));
+        
+        leftSpit.setIcon(new ImageIcon(swapLeftSpit));
+        rightSpit.setIcon(new ImageIcon(swapRightSpit));
+        
     }
     public DisplayImage() throws IOException
     {
         BufferedImage img = ImageIO.read(new File("cards.png"));
         
-        BufferedImage clubs1 = img.getSubimage(0, 0, 72, 96);
+        /* BufferedImage clubs1 = img.getSubimage(0, 0, 72, 96);
         BufferedImage clubs2 = img.getSubimage(72, 0, 72, 96);
         BufferedImage hearts1 = img.getSubimage(0 ,192, 72, 96);
         BufferedImage hearts2 = img.getSubimage(72, 192, 72, 96);
@@ -46,32 +97,32 @@ public class DisplayImage extends Deck
         
         BufferedImage diamonds10 = img.getSubimage(648, 288, 72, 96);
         BufferedImage hearts6 = img.getSubimage(360, 288, 72, 96);
-        
+        */
         JFrame frame = new JFrame();
         frame.setLayout(null);
         frame.setSize(750, 750);
         frame.getContentPane().setBackground(Color.GREEN);
         
         //JLabel player1 = new JLabel();
-        JLabel oneStockOne = new JLabel();
-        JLabel oneStockTwo = new JLabel();
-        JLabel oneStockThree = new JLabel();
-        JLabel oneStockFour = new JLabel();
-        JLabel oneStockFive = new JLabel();
+        //JLabel oneStockOne = new JLabel();
+        //JLabel oneStockTwo = new JLabel();
+        //JLabel oneStockThree = new JLabel();
+        //JLabel oneStockFour = new JLabel();
+        //JLabel oneStockFive = new JLabel();
         
-        JLabel player2 = new JLabel();
-        JLabel twoStockOne = new JLabel();
-        JLabel twoStockTwo = new JLabel();
-        JLabel twoStockThree = new JLabel();
-        JLabel twoStockFour = new JLabel();
-        JLabel twoStockFive = new JLabel();
+        //JLabel player2 = new JLabel();
+        //JLabel twoStockOne = new JLabel();
+        //JLabel twoStockTwo = new JLabel();
+        //JLabel twoStockThree = new JLabel();
+        //JLabel twoStockFour = new JLabel();
+        //JLabel twoStockFive = new JLabel();
         
-        JLabel leftSpit = new JLabel();
-        JLabel rightSpit = new JLabel();
+        //JLabel leftSpit = new JLabel();
+        //JLabel rightSpit = new JLabel();
         
         
         
-        player1.setIcon(new ImageIcon(clubs1));
+        /*player1.setIcon(new ImageIcon(clubs1));
         oneStockOne.setIcon(new ImageIcon(clubs2));
         oneStockTwo.setIcon(new ImageIcon(hearts1));
         oneStockThree.setIcon(new ImageIcon(hearts2));
@@ -88,7 +139,7 @@ public class DisplayImage extends Deck
         
         leftSpit.setIcon(new ImageIcon(diamonds10));
         rightSpit.setIcon(new ImageIcon(hearts6));
-       
+        */
         frame.add(player1);
         frame.add(oneStockOne);
         frame.add(oneStockTwo);
