d01a74617a29ed620800a8937475c299d60279a7
Benjamin Chin <benjamin.d.chin@gmail.com>
2019-06-14 09:11:14 -0400

    yo

--- KeyInput.java
+++ KeyInput.java
@@ -1,33 +1,51 @@
-
+import java.awt.event.*;
+import java.awt.*;
 /**
  * Write a description of class KeyInput here.
  *
- * @author (your name)
+ * @author (Ben Chin)
  * @version (a version number or a date)
  */
-public class KeyInput
+public class KeyInput implements KeyListener
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class KeyInput
-     */
-    public KeyInput()
+    private String pile;
+    private String spit;
+    public void keyPressed(KeyEvent e)
     {
-        // initialise instance variables
-        x = 0;
+        int c = e.getKeyCode();
+        if(c == KeyEvent.VK_1)
+            pile = "1";
+        else if(c == KeyEvent.VK_2)
+            pile = "2";
+        else if(c == KeyEvent.VK_3)
+            pile = "3";
+        else if(c == KeyEvent.VK_4)
+            pile = "4";
+        else if(c == KeyEvent.VK_5)
+            pile = "5";
+        else if(c == KeyEvent.VK_NUMPAD1)
+            pile = "num1";
+        else if(c == KeyEvent.VK_NUMPAD2)
+            pile = "num2";
+        else if(c == KeyEvent.VK_NUMPAD3)
+            pile = "num3";
+        else if(c == KeyEvent.VK_NUMPAD4)
+            pile = "num4";
+        else if(c == KeyEvent.VK_NUMPAD2)
+            pile = "num5";
+        else if(c == KeyEvent.VK_A || c == KeyEvent.VK_NUMPAD0)
+            spit = "left";
+        else if(c == KeyEvent.VK_D || c == KeyEvent.VK_DELETE)
+            spit = "right";
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public void keyTyped(KeyEvent e){}
+    public void keyReleased(KeyEvent e){}
+    public String pile()
     {
-        // put your code here
-        return x + y;
+        return pile;
+    }
+    public String spit()
+    {
+        return spit;
     }
 }
